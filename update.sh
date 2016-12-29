f67978d8d6ab14ec0118c4ffeaa17771e6c7a0a8
Rich Lafferty <rich@freshbooks.com>
2016-12-28 20:46:17 -0500

    Typo

--- update.sh
+++ update.sh
@@ -13,9 +13,11 @@ pip install gmusicapi==10.0.1
 cpanm --notest Inline
 INLINE_PYTHON_EXECUTABLE=/usr/bin/python2.7 cpanm --notest Inline::Python
 
+# "Async::HTTP: Unable to load IO::Socket::SSL, will try connecting to SSL servers in non-SSL mode"
+# what are you even doing over there, Async::HTTP? that won't work!
 cpanm --notest IO::Socket::SSL
 
-apt-get remove -qy libpython2.7-dev ibffi-dev make cpanminus libssl-dev
+apt-get remove -qy libpython2.7-dev libffi-dev make cpanminus libssl-dev
 apt-get autoremove -y
 apt-get clean -y
 rm -rf /var/lib/apt/lists/* /var/cache/* /var/tmp/*
